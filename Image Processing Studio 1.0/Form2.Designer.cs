6f0ee750443af04ef401b83c692666b396c783c8
Nisal Dilshan <bcnisal@gmail.com>
2017-03-14 19:30:50 +0530

    Use mouse to select Region of Interest and display result

--- Image Processing Studio 1.0/Form2.Designer.cs
+++ Image Processing Studio 1.0/Form2.Designer.cs
@@ -28,12 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBoxROI = new Emgu.CV.UI.ImageBox();
+            this.imageBoxOutputROI = new Emgu.CV.UI.ImageBox();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxROI)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxOutputROI)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -47,33 +53,65 @@
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
-            this.splitContainer1.Size = new System.Drawing.Size(804, 413);
-            this.splitContainer1.SplitterDistance = 521;
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.imageBoxROI);
+            this.splitContainer1.Panel2.Controls.Add(this.imageBoxOutputROI);
+            this.splitContainer1.Size = new System.Drawing.Size(884, 519);
+            this.splitContainer1.SplitterDistance = 601;
             this.splitContainer1.TabIndex = 0;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 3);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(521, 413);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
-            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.Size = new System.Drawing.Size(571, 504);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDoubleClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // imageBoxROI
+            // 
+            this.imageBoxROI.Location = new System.Drawing.Point(19, 190);
+            this.imageBoxROI.Name = "imageBoxROI";
+            this.imageBoxROI.Size = new System.Drawing.Size(248, 190);
+            this.imageBoxROI.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBoxROI.TabIndex = 2;
+            this.imageBoxROI.TabStop = false;
+            // 
+            // imageBoxOutputROI
+            // 
+            this.imageBoxOutputROI.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
+            this.imageBoxOutputROI.Location = new System.Drawing.Point(19, 12);
+            this.imageBoxOutputROI.Name = "imageBoxOutputROI";
+            this.imageBoxOutputROI.Size = new System.Drawing.Size(248, 172);
+            this.imageBoxOutputROI.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBoxOutputROI.TabIndex = 2;
+            this.imageBoxOutputROI.TabStop = false;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(804, 413);
+            this.ClientSize = new System.Drawing.Size(884, 519);
             this.Controls.Add(this.splitContainer1);
             this.Name = "Form2";
             this.Text = "CROP";
             this.Resize += new System.EventHandler(this.Form2_Resize);
             this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxROI)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxOutputROI)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -81,6 +119,8 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private System.Windows.Forms.PictureBox pictureBox1;
+        private Emgu.CV.UI.ImageBox imageBoxOutputROI;
+        private Emgu.CV.UI.ImageBox imageBoxROI;
+        private Emgu.CV.UI.ImageBox pictureBox1;
     }
 }
--- Image Processing Studio 1.0/Form2.cs
+++ Image Processing Studio 1.0/Form2.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Emgu.CV;
+using Emgu.CV.UI;
 using Emgu.CV.Util;
 using Emgu.CV.Structure;
 using Emgu.CV.CvEnum;
@@ -19,7 +20,7 @@ namespace Image_Processing_Studio_1._0
     public partial class Form2 : Form
     {
         UMat img;
-        Image displayImage;
+        Image<Bgr, byte> displayImage;
 
         public Form2()
         {
@@ -52,7 +53,7 @@ namespace Image_Processing_Studio_1._0
                 width = (int)Math.Round(height / Math.Tan(alpha));
             }
 
-            pictureBox1.Image = new Bitmap(displayImage, width, height);
+            pictureBox1.Image = displayImage;
         }
 
         private void redrawImg()
@@ -62,7 +63,7 @@ namespace Image_Processing_Studio_1._0
             if (img == null) return;
             try
             {
-                displayImage = img.ToImage<Bgr, byte>().ToBitmap();
+                displayImage = new Image<Bgr, byte>(img.ToImage<Bgr, byte>().ToBitmap());//img.ToImage<Bgr, byte>().ToBitmap();
 
                 updateDisplay();
             }
@@ -81,5 +82,155 @@ namespace Image_Processing_Studio_1._0
         {
 
         }
+
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\       
+        public static void ConvertCoordinates(PictureBox pic,out int X0, out int Y0, int x, int y)
+        {
+            int pic_hgt = pic.ClientSize.Height;
+            int pic_wid = pic.ClientSize.Width;
+            int img_hgt = pic.Image.Height;
+            int img_wid = pic.Image.Width;
+
+            X0 = x;
+            Y0 = y;
+            switch (pic.SizeMode)
+            {
+                case PictureBoxSizeMode.AutoSize:
+                case PictureBoxSizeMode.Normal:
+                    // These are okay. Leave them alone.
+                    break;
+                case PictureBoxSizeMode.CenterImage:
+                    X0 = x - (pic_wid - img_wid) / 2;
+                    Y0 = y - (pic_hgt - img_hgt) / 2;
+                    break;
+                case PictureBoxSizeMode.StretchImage:
+                    X0 = (int)(img_wid * x / (float)pic_wid);
+                    Y0 = (int)(img_hgt * y / (float)pic_hgt);
+                    break;
+                case PictureBoxSizeMode.Zoom:
+                    float pic_aspect = pic_wid / (float)pic_hgt;
+                    float img_aspect = img_wid / (float)img_wid;
+                    if (pic_aspect > img_aspect)
+                    {
+                        // The PictureBox is wider/shorter than the image.
+                        Y0 = (int)(img_hgt * y / (float)pic_hgt);
+
+                        // The image fills the height of the PictureBox.
+                        // Get its width.
+                        float scaled_width = img_wid * pic_hgt / img_hgt;
+                        float dx = (pic_wid - scaled_width) / 2;
+                        X0 = (int)((x - dx) * img_hgt / (float)pic_hgt);
+                    }
+                    else
+                    {
+                        // The PictureBox is taller/thinner than the image.
+                        X0 = (int)(img_wid * x / (float)pic_wid);
+
+                        // The image fills the height of the PictureBox.
+                        // Get its height.
+                        float scaled_height = img_hgt * pic_wid / img_wid;
+                        float dy = (pic_hgt - scaled_height) / 2;
+                        Y0 = (int)((y - dy) * img_wid / pic_wid);
+                    }
+                    break;
+            }  
+        }
+        //\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+        #region EVENTOS PICTURE BOX | DEFINIÇÃO DE ROI
+        private Image<Bgr, byte> imgEntrada;
+        private Point RectStartPoint;
+        private Rectangle Rect = new Rectangle();
+        private Rectangle RealImageRect = new Rectangle();
+        private Brush selectionBrush = new SolidBrush(Color.FromArgb(128, 64, 64, 64));
+        private int thickness = 3;
+
+
+
+        private void pictureBox1_MouseDown(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            // Determine the initial rectangle coordinates...
+            RectStartPoint = e.Location;
+            Invalidate();
+        }
+
+
+        private void pictureBox1_MouseMove(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            #region SETS COORDINATES AT INPUT IMAGE BOX
+            int X0, Y0;
+            ConvertCoordinates(pictureBox1, out X0, out Y0, e.X, e.Y);
+            //labelPostionXY.Text = "Last Position: X:" + X0 + "  Y:" + Y0;
+
+            //Coordinates at input picture box
+            if (e.Button != MouseButtons.Left)
+                return;
+            Point tempEndPoint = e.Location;
+            Rect.Location = new Point(
+                Math.Min(RectStartPoint.X, tempEndPoint.X),
+                Math.Min(RectStartPoint.Y, tempEndPoint.Y));
+            Rect.Size = new Size(
+                Math.Abs(RectStartPoint.X - tempEndPoint.X),
+                Math.Abs(RectStartPoint.Y - tempEndPoint.Y));
+            #endregion
+
+            #region SETS COORDINATES AT REAL IMAGE
+            //Coordinates at real image - Create ROI
+            ConvertCoordinates(pictureBox1, out X0, out Y0,
+            RectStartPoint.X, RectStartPoint.Y);
+            int X1, Y1;
+            ConvertCoordinates(pictureBox1, out X1, out Y1, tempEndPoint.X, tempEndPoint.Y);
+            RealImageRect.Location = new Point(
+                Math.Min(X0, X1),
+                Math.Min(Y0, Y1));
+            RealImageRect.Size = new Size(
+                Math.Abs(X0 - X1),
+                Math.Abs(Y0 - Y1));
+
+           
+            imgEntrada = new Image<Bgr, byte>(img.ToImage<Bgr, byte>().ToBitmap());
+            imgEntrada.Draw(RealImageRect, new Bgr(Color.Red), thickness);
+            imageBoxOutputROI.Image = imgEntrada;
+            #endregion
+
+            ((PictureBox)sender).Invalidate();
+        }
+
+
+        private void pictureBox1_Paint(object sender, System.Windows.Forms.PaintEventArgs e)
+        {
+            // Draw the rectangle...
+            if (pictureBox1.Image != null)
+            {
+                if (Rect != null && Rect.Width > 0 && Rect.Height > 0)
+                {
+                    //Seleciona a ROI
+                    e.Graphics.SetClip(Rect, System.Drawing.Drawing2D.CombineMode.Exclude);
+                    e.Graphics.FillRectangle(selectionBrush, new Rectangle
+            (0, 0, ((PictureBox)sender).Width, ((PictureBox)sender).Height));
+                    //e.Graphics.FillRectangle(selectionBrush, Rect);
+                }
+            }
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            //Define ROI. Valida altura e largura para evitar index range exception.
+            if (RealImageRect.Width > 0 && RealImageRect.Height > 0)
+            {
+                imgEntrada.ROI = RealImageRect;
+                imageBoxROI.Image = imgEntrada;
+            }
+
+        }
+
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            Rect = new Rectangle();
+            ((PictureBox)sender).Invalidate();
+        }
+
+        #endregion
+
+        
     }
 }
