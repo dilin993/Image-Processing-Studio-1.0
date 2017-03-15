07b2565a4d7dd5d97976f127837fe941ad186124
Nisal Dilshan <bcnisal@gmail.com>
2017-03-15 09:54:42 +0530

    Fixed image boxes

--- Image Processing Studio 1.0/Form2.Designer.cs
+++ Image Processing Studio 1.0/Form2.Designer.cs
@@ -32,21 +32,20 @@
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
             this.pictureBox1 = new Emgu.CV.UI.ImageBox();
             this.imageBoxROI = new Emgu.CV.UI.ImageBox();
-            this.imageBoxOutputROI = new Emgu.CV.UI.ImageBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBoxROI)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBoxOutputROI)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainer1
             // 
             this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
-            this.splitContainer1.IsSplitterFixed = true;
             this.splitContainer1.Location = new System.Drawing.Point(0, 0);
             this.splitContainer1.Name = "splitContainer1";
             // 
@@ -56,19 +55,21 @@
             // 
             // splitContainer1.Panel2
             // 
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.button1);
             this.splitContainer1.Panel2.Controls.Add(this.imageBoxROI);
-            this.splitContainer1.Panel2.Controls.Add(this.imageBoxOutputROI);
-            this.splitContainer1.Size = new System.Drawing.Size(884, 519);
-            this.splitContainer1.SplitterDistance = 601;
+            this.splitContainer1.Size = new System.Drawing.Size(887, 519);
+            this.splitContainer1.SplitterDistance = 513;
             this.splitContainer1.TabIndex = 0;
             // 
             // pictureBox1
             // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.pictureBox1.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(571, 504);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.Size = new System.Drawing.Size(513, 519);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
@@ -79,22 +80,30 @@
             // 
             // imageBoxROI
             // 
-            this.imageBoxROI.Location = new System.Drawing.Point(19, 190);
+            this.imageBoxROI.Location = new System.Drawing.Point(13, 12);
             this.imageBoxROI.Name = "imageBoxROI";
-            this.imageBoxROI.Size = new System.Drawing.Size(248, 190);
-            this.imageBoxROI.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBoxROI.Size = new System.Drawing.Size(342, 310);
+            this.imageBoxROI.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.imageBoxROI.TabIndex = 2;
             this.imageBoxROI.TabStop = false;
             // 
-            // imageBoxOutputROI
+            // button1
             // 
-            this.imageBoxOutputROI.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Minimum;
-            this.imageBoxOutputROI.Location = new System.Drawing.Point(19, 12);
-            this.imageBoxOutputROI.Name = "imageBoxOutputROI";
-            this.imageBoxOutputROI.Size = new System.Drawing.Size(248, 172);
-            this.imageBoxOutputROI.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            this.imageBoxOutputROI.TabIndex = 2;
-            this.imageBoxOutputROI.TabStop = false;
+            this.button1.Location = new System.Drawing.Point(70, 399);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(217, 398);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form2
             // 
@@ -111,7 +120,6 @@
             this.splitContainer1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.imageBoxROI)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.imageBoxOutputROI)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -119,8 +127,9 @@
         #endregion
 
         private System.Windows.Forms.SplitContainer splitContainer1;
-        private Emgu.CV.UI.ImageBox imageBoxOutputROI;
         private Emgu.CV.UI.ImageBox imageBoxROI;
         private Emgu.CV.UI.ImageBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
     }
 }
